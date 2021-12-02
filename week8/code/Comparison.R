943447323f18aeef29bcc6303c792aeffb62bbcb
jasmineby65 <zy1921@ic.ac.uk>
2021-12-02 00:46:17 +0000

    Regular upload

--- week8/code/Comparison.R
+++ week8/code/Comparison.R
@@ -1,4 +1,6 @@
 data <- read.csv("../data/Data.csv", stringsAsFactors = F) 
+data["LogP"] <- log(data$PopBio)
+head(data)
 
 # Subset single experiments
 subset <- subset(data, ID == "1")
@@ -7,3 +9,57 @@ head(subset)
 
 plot(subset$Time, subset$PopBio)
 plot(subset$Time, subset$LogP)
+
+require(dplyr)
+results <- data %>%
+  group_by(ID) %>%
+  do(Modelling(.))
+
+results
+Finals <- data.frame(results)
+Finals
+
+Finals <- data.frame(matrix(ncol = 5, nrow = 0))
+x <- c("Models", "Type", "Category","AIC", "ID")
+colnames(Finals) <- x
+Finals
+
+
+for(i in 1:max(data$ID)){
+  subset <- subset(data, ID == i)
+  Models <- try(Modelling(subset),silent = T)
+  Models$ID = i
+  Finals <- rbind(Finals, Models)
+}
+Finals
+tail(Finals)
+str(Finals)
+
+subset <- subset(data, ID == 161)
+estimate_r(subset)
+plot(subset$Time, subset$PopBio)
+
+rolling <- roll_regres(PopBio ~ Time, data = subset, width = as.integer(nrow(subset)*0.45)) 
+rolling
+a <- which.max(rolling$coefs[,"Time"])
+r_estimate <- rolling$coefs[a,"Time"]
+r_estiamte
+
+Models <- Modelling(subset)
+Models
+Models$ID = 160
+Finals <- rbind(Finals, Models)
+
+
+for(i in 0:max(data$ID)){
+  subset <- subset(data, ID == i)
+  models <- subset(Finals, ID == i)
+  plot <- plotting(subset, models)
+  
+  
+  jpeg(paste0("../plots/Subset", i, ".jpg"), 800, 500)
+  print(plot)
+  dev.off()
+  
+  cat("Plot", i, "saved!")
+}
--- week8/code/Modelling.R
+++ week8/code/Modelling.R
@@ -40,14 +40,25 @@ cube_modelling(subset)
 
 estimate_r <- function(subset){
   require(rollRegres)
-  rolling <- roll_regres(PopBio ~ Time, data = subset, width = as.integer(nrow(subset)*0.45)) 
+  rolling <- try(roll_regres(PopBio ~ Time, data = subset, width = as.integer(nrow(subset)*0.45)), silent = T)
+  
+  if(class(rolling) == "try-error" | is.null(rolling)){
+    
+    lm_growth <- lm(PopBio ~ Time, data = subset)
+    r_estimate <- coef(summary(lm_growth))["Time","Estimate"]
+    r_estimate
+    return(r_estimate)  
+
+      } else {
+    
   a <- which.max(rolling$coefs[,"Time"])
   r_estimate <- rolling$coefs[a,"Time"]
   return(r_estimate)
-}
+  
+}}
 
-estimate_r(subset)
-
+  r <- estimate_r(subset)
+r
 
 # 3. Logistic model # 
 
@@ -60,9 +71,15 @@ logistic_model <- function(t, r_max, K, N_0){
 grid_search_logistic <- function(subset){
   r_estimate <- estimate_r(subset)
   
-  r_max_start <- rnorm(100, r_estimate, r_estimate*2)
-  N_0_start <- rnorm(100, min(subset$PopBio), min(subset$PopBio)*0.5) 
-  K_start <- rnorm(100, max(subset$PopBio), min(subset$PopBio)*0.5) 
+  if(r_estimate > 3 | r_estimate < 0){
+    r_max_start <- runif(100, 0, 0.0005)
+  } else {r_max_start <- rnorm(100, r_estimate, r_estimate*2)}
+  
+  if(min(subset$PopBio)==0){N_0_start <- rep(0, 100)} else {
+    N_0_start <- rep(min(subset$PopBio), 100) 
+  }
+  
+  K_start <- rep(max(subset$PopBio), 100) 
   
   combination <- data.frame(r_max_start, N_0_start, K_start)
   names(combination) <- c("r_max", "N_0", "K")
@@ -82,23 +99,36 @@ Logistic_models <- grid_search_logistic(subset)
 
 AIC_calculation <- function(x){
   require(wiqid)
-  try(AICc(x), silent = T)
+  if(class(x) == "lm" | class(x) == "nls"){
+    return(AICc(x))
+  } else {
+    return(NA)
+  }
 }
 
 
 best_logistic <- function(subset){
   
   Logistic_models <- grid_search_logistic(subset)
-  
+  Logistic_models
   Logistic_models$AIC <- lapply(Logistic_models, AIC_calculation)
-  best <- which.min(Logistic_models$AIC)
+  Logistic_models$AIC
   
-  return(Logistic_models[[best]])
-}
+  if(length(unique(Logistic_models$AIC))==1){
+    best_log = NA
+    return(best_log)
+  } else {  
+  best <- which.min(Logistic_models$AIC)
+  best_log <- Logistic_models[[best]]
+  best_log
+  return(best_log)
+}}
 
 best_log <- best_logistic(subset)
+best_log
 
 
+  
 # 4. Gompertz model #
 # Takes lag phase at the beginning of growth into account
 # This model is designed specifically to be fitted to log-transformed data
@@ -110,10 +140,18 @@ gompertz_model <- function(t, r_max, K, N_0, t_lag){ # Modified gompertz growth 
 grid_search_gompertz <- function(subset){
   r_estimate <- estimate_r(subset)
   
-  r_max_start <- rnorm(100, r_estimate, r_estimate*2)
-  N_0_start <- rnorm(100, min(subset$LogP), abs(min(subset$LogP)*0.5)) 
-  K_start <- rnorm(100, max(subset$LogP), abs(min(subset$LogP)*0.5))
-  t_lag_start <- runif(100, 0, max(subset$Time)*0.1)
+  if(r_estimate > 3 | r_estimate < 0){
+    r_max_start <- runif(100, 0, 0.0005)
+  } else {r_max_start <- rnorm(100, r_estimate, r_estimate*2)}
+  
+  if(min(subset$PopBio)==0){N_0_start <- rep(-10, 100)} else {
+    N_0_start <- rep(min(subset$LogP), 100) 
+  }
+  
+  K_start <- rep(max(subset$LogP), 100)
+  t_estimate <- subset$Time[which.max(diff(diff(subset$LogP)))]
+  t_lag_start <- rnorm(100, t_estimate, t_estimate*2)
+ # t_lag_start <- runif(100, 0, max(subset$Time)*0.1)
   
   combination <- data.frame(r_max_start, N_0_start, K_start, t_lag_start)
   names(combination) <- c("r_max", "N_0", "K", "t_lag")
@@ -122,44 +160,57 @@ grid_search_gompertz <- function(subset){
   require(minpack.lm) 
   fit_gompertz <- function(x){
     try(nlsLM(LogP ~ gompertz_model(t = Time, r_max, K, N_0, t_lag), subset,
-              list(t_lag=x[4], r_max=x[1], N_0 = x[2], K = x[3])), silent = T)}
+              list(t_lag=x[4], r_max=x[1], N_0 = x[2], K = x[3]), lower = c(0,-Inf,-Inf,0)), silent = T)}
   
   Gompertz_models <- apply(combination, 1, fit_gompertz)
   
   return(Gompertz_models)
 }
 
+
 best_gompertz <- function(subset){
   
   Gompertz_models <- grid_search_gompertz(subset)
+  Gompertz_models
+  
+  class(Gompertz_models[[1]])
   
   Gompertz_models$AIC <- lapply(Gompertz_models, AIC_calculation)
-  best <- which.min(Gompertz_models$AIC)
+  Gompertz_models$AIC
   
-  return(Gompertz_models[[best]])
-}
+  if(length(unique(Gompertz_models$AIC))==1){
+    best_gomp = NA
+    return(best_gomp)
+  } else {  
+    best <- try(which.min(Gompertz_models$AIC), silent = T)
+    best
+    best_gomp <- Gompertz_models[[best]]
+    best_gomp
 
-AICc(best_gompertz(subset))
+    return(best_gomp)}}
 
 best_gom <- best_gompertz(subset)
-
+best_gom
 
 # Modelling all four types #
 Modelling <- function(subset){
   Models <- list(quad_modelling(subset), cube_modelling(subset), best_logistic(subset), best_gompertz(subset))
   Type <- c("Quadratic", "Cubic", "Logistic", "Gompertz")
   Category <- c("Phenomenological","Phenomenological","Mechanistic", "Mechanistic")
+  ID <- rep(unique(subset$ID),4)
   
-  df <- data.frame(I(Models), Type, Category)
+  df <- data.frame(I(Models), Type, Category, ID)
+  
   df$AIC <- lapply(Models, AIC_calculation)
   return(df)
   }
 
 df <- Modelling(subset)
+df
 
 # Plotting #
 
-plotting <- function(subset, df){
+  plotting <- function(subset, df){
   
   time <- data.frame(subset$Time)
   
@@ -173,44 +224,49 @@ plotting <- function(subset, df){
   names(df2) <- c("Time", "LogN", "model")
   head(df2)
   
-  timepoints <- seq(0, max(subset$Time), 1)
+  model_frame <- rbind(df1, df2)
   
-  logistic_points <- logistic_model(t = timepoints, 
+  if(is.na(df$Models[[3]])==F){
+    timepoints <- seq(0, max(subset$Time), 1)  
+  
+    logistic_points <- logistic_model(t = timepoints, 
                                     r_max = summary(df$Models[[3]])$coefficients["r_max.r_max","Estimate"], 
                                     N_0 = summary(df$Models[[3]])$coefficients["N_0.N_0","Estimate"], 
                                     K = summary(df$Models[[3]])$coefficients["K.K","Estimate"])
-  df3 <- data.frame(timepoints, log(logistic_points))
-  df3$model <- "Logistic equation"
-  names(df3) <- c("Time", "LogN", "model")
-  head(df3)
+    df3 <- data.frame(timepoints, log(logistic_points))
+    df3$model <- "Logistic equation"
+    names(df3) <- c("Time", "LogN", "model")
+    head(df3) 
+    model_frame <- rbind(model_frame, df3)
+    } 
   
-  gompertz_points <- gompertz_model(t = timepoints, 
+  if(is.na(df$Models[[4]])==F){
+    timepoints <- seq(0, max(subset$Time), 1)  
+    
+    gompertz_points <- gompertz_model(t = timepoints, 
                                     r_max = summary(df$Models[[4]])$coefficients["r_max.r_max","Estimate"], 
                                     N_0 = summary(df$Models[[4]])$coefficients["N_0.N_0","Estimate"], 
                                     K = summary(df$Models[[4]])$coefficients["K.K","Estimate"],
                                     t_lag = summary(df$Models[[4]])$coefficients["t_lag.t_lag","Estimate"])
   
-  df4 <- data.frame(timepoints, gompertz_points)
-  df4$model <- "Gompertz model"
-  names(df4) <- c("Time", "LogN", "model")
-  head(df4)
-  
-  model_frame <- rbind(df1, df2, df3, df4)
+    gompertz_points
+    df4 <- data.frame(timepoints, gompertz_points)
+    df4$model <- "Gompertz model"
+    names(df4) <- c("Time", "LogN", "model")
+    head(df4)
+    model_frame <- rbind(model_frame, df4)}
   
   require(ggplot2)
   model_plot <- ggplot(subset, aes(x = Time, y = LogP)) +
-    geom_point(size = 5) +
+    geom_point(size = 3) +
     geom_line(data = model_frame, aes(x = Time, y = LogN, col = model), size = 1.5) +
     labs(x = "Time", y = "log(Population)") +
     scale_color_brewer(palette="Set2") +
-    theme_bw() 
-  theme(legend.position = "none",
-        panel.grid.minor = element_blank()) 
-  theme(aspect.ratio=1)
+    theme_bw() +
+  theme(panel.grid.minor = element_blank()) +
+  theme(aspect.ratio=1) +
+    ggtitle(paste0(subset$ID))
   
   return(model_plot)
-}
-
-plotting(subset, df)
-
-
+  }
+  
