729ea5db805fb44bcc1c9154a93f376f0c20c7c1
Jasmine <zy1921@ic.ac.uk>
2022-07-20 09:29:00 +0100

    backup

--- Project/R_shape_to_python.R
+++ Project/R_shape_to_python.R
@@ -5,6 +5,7 @@ getwd()
 setwd("~/Documents/CMEECourseWork/Project/")
 
 writeOGR(obj = orsifronts, dsn = "Shapes", layer = "SO_fronts", driver="ESRI Shapefile")
+writeOGR(obj = parkfronts, dsn = "Shapes", layer = "SO_fronts_Park", driver = "ESRI Shapefile")
 # driver specifies the format of output shape file
 # dsn = destination directory
 # layer = output file name
